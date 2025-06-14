db83a02821e7fbf911ea838e9163f97ce4840413
liuzhi <1759586066@qq.com>
2025-06-14 22:32:45 +0800

    添加公司介绍接口

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, App, HttpServer, HttpResponse};
 use reqwest::Client;
-use crate::services::stock_service::get_stock_data;
+use crate::services::stock_service::{get_company_data, get_stock_data};
 
 mod core;
 mod services;
@@ -11,7 +11,8 @@ async fn main() -> std::io::Result<()> {
     // 启动 HTTP 服务器并绑定到指定地址
     HttpServer::new(|| {
         App::new()
-            .route("/stock/{symbol}", web::get().to(stock_handler)) // 设置路由
+            .route("/stock/{symbol}", web::get().to(stock_handler)) 
+            .route("/company/{symbol}", web::get().to(company_handler))
     })
         .bind("127.0.0.1:8080")? // 监听地址和端口
         .run()
@@ -38,6 +39,30 @@ async fn stock_handler(path: web::Path<(String,)>) -> HttpResponse {
         Err(err) => {
             eprintln!("请求失败: {}", err);
             HttpResponse::InternalServerError().body(format!("请求失败: {}", err)) // 返回 500 错误
+        }
+    }
+}
+
+
+async fn company_handler(path: web::Path<(String,)>) -> HttpResponse {
+    let symbol = &path.0;  // 解构 path 中的元组，直接获取 symbol 参数
+
+    // 创建 reqwest 客户端
+    let client = Client::new();
+
+    // 获取股票数据
+    match get_company_data(symbol, &client).await {
+        Ok(data) => {
+            // 返回结果作为 JSON 响应
+            if let Some(company) = data.get("data").and_then(|data| data.get("company")) {
+                HttpResponse::Ok().json(company) 
+            } else {
+                HttpResponse::NotFound().body("未找到 company 字段") 
+            }
+        }
+        Err(err) => {
+            eprintln!("请求失败: {}", err);
+            HttpResponse::InternalServerError().body(format!("请求失败: {}", err)) 
         }
     }
 }
--- src/services/stock_service.rs
+++ src/services/stock_service.rs
@@ -3,12 +3,10 @@ use serde_json::Value;
 use crate::errors::error::{MyError};
 use crate::core::common_header::create_headers;
 
-pub async fn get_stock_data(symbol: &str, client: &Client) -> Result<Value, MyError> {
-    let url = format!("https://stock.xueqiu.com/v5/stock/quote.json?symbol={}", symbol);
-
+async fn get_data_from_url(url: &str, client: &Client) -> Result<Value, MyError> {
     // 发送 HTTP 请求
     let response = client
-        .get(&url)
+        .get(url)
         .headers(create_headers())
         .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
         .send()
@@ -36,4 +34,16 @@ pub async fn get_stock_data(symbol: &str, client: &Client) -> Result<Value, MyEr
 
     // 返回解析后的 JSON 数据
     Ok(json_data)
+}
+
+// 获取股票数据
+pub async fn get_stock_data(symbol: &str, client: &Client) -> Result<Value, MyError> {
+    let url = format!("https://stock.xueqiu.com/v5/stock/quote.json?symbol={}", symbol);
+    get_data_from_url(&url, client).await
+}
+
+// 获取公司数据
+pub async fn get_company_data(symbol: &str, client: &Client) -> Result<Value, MyError> {
+    let url = format!("https://stock.xueqiu.com/v5/stock/f10/cn/company.json?symbol={}", symbol);
+    get_data_from_url(&url, client).await
 }
